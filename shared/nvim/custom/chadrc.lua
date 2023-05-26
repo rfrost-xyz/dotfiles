6fc49d0ff35b2c7c4a7fb5f8fc0838d4513602fa
Richard Frost <richard@rfrost.xyz>
2023-05-26 07:37:25 +0100

    Parity with Qubes OS

--- shared/nvim/custom/chadrc.lua
+++ shared/nvim/custom/chadrc.lua
@@ -9,6 +9,15 @@ M.ui = {
   theme_toggle = { "chadracula", "github_dark" },
   hl_override = highlights.override,
   hl_add = highlights.add,
+
+  statusline = {
+    theme = "default", -- default/vscode/vscode_colored/minimal
+    -- default/round/block/arrow (separators work only for "default" statusline theme;
+    -- round and block will work for the minimal theme only)
+    separator_style = "default",
+    -- overriden_modules = nil,
+  },
+
 }
 
 M.plugins = "custom.plugins"
@@ -17,4 +26,3 @@ M.plugins = "custom.plugins"
 M.mappings = require "custom.mappings"
 
 return M
-
